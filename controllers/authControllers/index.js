cbf2e319a50947b893ddd512ec96aeced0e2ea61
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-04-12 16:07:55 +0300

    remove commented code

--- controllers/authControllers/index.js
+++ controllers/authControllers/index.js
@@ -7,14 +7,6 @@ import { updateUserSubscription } from "./updateUserSubscription.js";
 import { updateUserAvatar } from "./updateUserAvatar.js";
 import { verify } from "./verify.js";
 import { resendVerify } from "./resendVerify.js";
-// export { default as signup } from "./signup.js";
-// export { default as signin } from "./signin.js";
-// export { default as getCurrent } from "./getCurrent.js";
-// export { default as signout } from "./signout.js";
-// export { default as updateUserSubscription } from "./updateUserSubscription.js";
-// export { default as updateUserAvatar } from "./updateUserAvatar.js";
-// export { default as verify } from "./verify.js";
-// export { default as resendVerify } from "./resendVerify.js";
 
 export default {
   signup: ctrlWrapper(signup),
